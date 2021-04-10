cb6f692ef87e470f11369994692688600b1f47da
David <david.davidyao.yao@gmail.com>
2021-04-10 23:17:27 +0800

    basic bot

---
+++ tracker.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def get_prices():
+    coins = ["BTC", "ETH", "XRP", "LTC", "BCH", "ADA", "DOT", "LINK", "BNB", "XLM"]
+
+    crypto_data = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms=USD".format(",".join(coins))).json()["RAW"]
+
+    data = {}
+    for i in crypto_data:
+        data[i] = {
+            "coin": i,
+            "price": crypto_data[i]["USD"]["PRICE"],
+            "change_day": crypto_data[i]["USD"]["CHANGEPCT24HOUR"],
+            "change_hour": crypto_data[i]["USD"]["CHANGEPCTHOUR"]
+        }
+
+    return data
+
+
+if __name__ == "__main__":
+    print(get_prices())
