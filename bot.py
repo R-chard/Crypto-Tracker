3e482075fed6ad2e822bd7625a71bfea21b5af49
richard <richardding1999@gmail.com>
2021-04-11 12:06:50 +0100

    Added threading

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import requests
 import telegram
+import _thread as thread
+import time
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from tracker import get_prices, get_top_coins,get_graph_info
 
@@ -7,6 +9,9 @@ telegram_bot_token = "1710250957:AAHpexQYf2Sp3aFOoeXmuQbEe0opwA9F9Dw"
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
+
+# chat_id: {coin,isAbove,price}
+activeAlerts = {}
 
 def get(update, context):
     chat_id = update.effective_chat.id
@@ -53,6 +58,43 @@ def top(update,context):
     except Exception as e:
         context.bot.send_message(chat_id=chat_id,text="Error")
 
+# polling function to be ran on a seperate thread
+def thread_poller(threadName,context):
+    # Time in seconds to delay thread
+    DELAY = 5
+    global activeAlerts
+    while True:
+        time.sleep(DELAY)
+        for chat_id,(coin,isAbove,threshold_price) in list(activeAlerts.items()):
+            data = get_prices(coin)
+            currentPrice = float(data["price"][1:].replace(',','')) # First character is the dollar sign
+            if isAbove:
+                if currentPrice > threshold_price:
+                    message = f"Price of {data['ticker']} is now at {data['price']} and above your threshold price of ${threshold_price}"
+                    context.bot.send_message(chat_id=chat_id,text=message)
+                    del activeAlerts[chat_id]
+            elif currentPrice < threshold_price:
+                message = f"Price of {data['ticker']} is now at {data['price']} and below your threshold price of ${threshold_price}"
+                context.bot.send_message(chat_id=chat_id,text=message)
+                del activeAlerts[chat_id]
+
+def alert(update,context):
+    chat_id = update.effective_chat.id
+    text = update.message.text.split()
+    try:
+        if text[2].lower() == "above":
+            isAbove = True
+        elif text[2].lower() == "below":
+            isAbove = False
+        else:
+            raise ValueError
+        
+        threshold_price = float(text[3])
+
+        activeAlerts[chat_id]=(text[1],isAbove,threshold_price)
+        thread.start_new_thread(thread_poller,("T1",context))
+    except Exception:
+        context.bot.send_message(chat_id=chat_id,text="Invalid input format")
 
 def start(update, context):
     chat_id = update.effective_chat.id
@@ -63,4 +105,5 @@ dispatcher.add_handler(CommandHandler("start", start)) # links /start with the s
 dispatcher.add_handler(CommandHandler("get", get)) # links /get with the get function
 dispatcher.add_handler(CommandHandler("top", top))
 dispatcher.add_handler(CommandHandler("photo", photo))
+dispatcher.add_handler(CommandHandler("alert",alert))
 updater.start_polling()
